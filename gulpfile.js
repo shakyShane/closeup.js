bcd1b9e1061e36431763eef8a5115c16814082b6
Shane Osbourne <shane.osbourne8@gmail.com>
2014-06-07 23:08:52 +0100

    added IE compatible

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,27 @@ var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
 var rename = require("gulp-rename");
 
-gulp.task("build", function () {
-    return gulp.src(["bower_components/norman.js/lib/norman.js", "lib/closeup.js"])
+var src = {
+    lib: ["bower_components/norman.js/lib/norman.js", "lib/closeup.js"]
+};
+
+gulp.task("build-lib", function () {
+    return gulp.src(src.lib)
         .pipe(concat("closeup.js"))
         .pipe(gulp.dest("./dist"))
         .pipe(uglify())
         .pipe(rename("closeup.min.js"))
+        .pipe(gulp.dest("./dist"));
+});
+gulp.task("build-poly", function () {
+
+    src.lib.unshift("polyfills/addEventListener.js")
+
+    return gulp.src(src.lib)
+        .pipe(concat("closeup.ie.js"))
+        .pipe(gulp.dest("./dist"))
+        .pipe(uglify())
+        .pipe(rename("closeup.ie.min.js"))
         .pipe(gulp.dest("./dist"));
 });
 
