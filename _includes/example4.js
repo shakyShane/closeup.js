7266052a8c1294ecfce1f16dcc7ae066c9d666c1
Shane Osbourne <shane.osbourne8@gmail.com>
2014-06-10 00:00:36 +0100

    Added example 4

---
+++ _includes/example4.js
@@ -0,0 +1,20 @@
+var zoomer =
+    new Closeup("#example1", "#img1", {loadDelay: 2000});
+
+var button   = document.getElementById("startZoom");
+
+// Add listener to button
+button.addEventListener("click", function (evt) {
+
+    evt.preventDefault();
+
+    // Set waiting text on button
+    button.innerText = "Loading, Please Wait...";
+
+    // Set the zoom image, and reset the button the text when loaded
+    zoomer.setZoomImage("/images/image1-large.jpg", function () {
+        button.innerText = "Ready :)";
+    });
+
+}, false);
+
